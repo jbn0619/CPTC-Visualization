bec6f8b71ebc791da5a8808b078259ed12b2d5ee
jbn0619 <jbn5379@g.rit.edu>
2021-01-26 10:55:49 -0500

    Removed Input System Package
    Removed Input System Package and added in basic class structs for use when generating JSON data.

--- Unity Project/CPTC Visualizer/Assets/Scripts/JSONWriter.cs
+++ Unity Project/CPTC Visualizer/Assets/Scripts/JSONWriter.cs
@@ -27,8 +27,15 @@ public class JSONWriter: MonoBehaviour
     [SerializeField]
     private int maxNodes;
 
+    // This is in alerts per team.
     [SerializeField]
     private int maxAlerts;
+
+    private int teamCount;
+    private int nodeCount;
+
+    // This is in alerts per team.
+    private int alertCount;
     
     #endregion Fields
     
@@ -40,7 +47,7 @@ public class JSONWriter: MonoBehaviour
     void Start()
     {
         // Alerts hold event types
-        
+
         // Teams hold discovered node IDs and alerts
 
         // Entire infrastructure holds nodes
@@ -59,10 +66,17 @@ public class JSONWriter: MonoBehaviour
     /// </summary>
     public void GenerateData()
     {
+        // Set up how much of each data type there will be.
+        alertCount = Random.Range(1, maxAlerts);
+        teamCount = Random.Range(1, maxTeams);
+        nodeCount = Random.Range(1, maxNodes);
+
         // First, check if we're randomizing the node infrastructure or not.
         if (randomizeInfrastructure == false)
         {
             // Generate a standard infrastructure of 5 nodes.
+            nodeCount = 5;
+
 
         }
         else
---
+++ Unity Project/CPTC Visualizer/Assets/Scripts/JsonClasses.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class JsonClasses
+    {
+    }
+
+    [System.Serializable]
+    public class NetworkNode
+    {
+        public int id;
+
+        public NodeTypes type;
+
+        public List<int> connections;
+
+        public static NetworkNode CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<NetworkNode>(jsonString);
+        }
+    }
+
+    [System.Serializable]
+    public class Alert
+    {
+        public CPTCEvents type;
+
+        public static Alert CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<Alert>(jsonString);
+        }
+    }
+
+    [System.Serializable]
+    public class Team
+    {
+        public List<Alert> alerts;
+
+        public static List<NetworkNode> discoveredNodes;
+
+        public Team CreateFromJSON(string jsonString)
+        {
+            return JsonUtility.FromJson<Team>(jsonString);
+        }
+    }
+}
