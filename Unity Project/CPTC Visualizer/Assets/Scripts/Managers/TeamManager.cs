fabea5c78a4a7fcbb3d07723b713cf88fc422a49
Kevin Laporte <kjl8898@rit.edu>
2021-03-03 13:35:53 -0500

    Added Comments and Fixed TeamName Generation

--- Unity Project/CPTC Visualizer/Assets/Scripts/Managers/TeamManager.cs
+++ Unity Project/CPTC Visualizer/Assets/Scripts/Managers/TeamManager.cs
@@ -242,76 +242,76 @@ public abstract class TeamManager : MonoBehaviour
         teamViewButtons.Clear();
     }
 
-    // Have this return a List<string>
-    public virtual void GenerateTeamNames()
-    {
-        List<string> potentialNames = new List<string>();
-        List<string> teamNames = new List<string>();
-        List<string> teamColors = new List<string>();
+    //// Have this return a List<string>
+    //public virtual void GenerateTeamNames()
+    //{
+    //    List<string> potentialNames = new List<string>();
+    //    List<string> teamNames = new List<string>();
+    //    List<string> teamColors = new List<string>();
 
-        // Generates a list of potential names from the file
-        StreamReader reader = new StreamReader("Assets/Data/animalNames.txt");
-        while (reader.Peek() != -1)
-        {
-            potentialNames.Add(reader.ReadLine());
-        }
-        reader.Close();
+    //    // Generates a list of potential names from the file
+    //    StreamReader reader = new StreamReader("Assets/Data/animalNames.txt");
+    //    while (reader.Peek() != -1)
+    //    {
+    //        potentialNames.Add(reader.ReadLine());
+    //    }
+    //    reader.Close();
 
-        // Gets a random index, adds it to the team names, and removes it from
-        //      potential names. Then creates a random color for the team.
-        for (int i = 0; i < teams.Count; i++)
-        {
-            int index = Random.Range(0, potentialNames.Count);
-            teamNames.Add(potentialNames[index]);
-            potentialNames.RemoveAt(index);
+    //    // Gets a random index, adds it to the team names, and removes it from
+    //    //      potential names. Then creates a random color for the team.
+    //    for (int i = 0; i < teams.Count; i++)
+    //    {
+    //        int index = Random.Range(0, potentialNames.Count);
+    //        teamNames.Add(potentialNames[index]);
+    //        potentialNames.RemoveAt(index);
 
-            Color color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
-            // team[i].color = color
-            teamColors.Add("#" + ColorUtility.ToHtmlStringRGBA(color));
-            //Debug.Log("Generated color: " + teamColors[i]);
-        }
+    //        Color color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+    //        // team[i].color = color
+    //        teamColors.Add("#" + ColorUtility.ToHtmlStringRGBA(color));
+    //        //Debug.Log("Generated color: " + teamColors[i]);
+    //    }
 
-        // Writes the selected team names and colors to a file
-        StreamWriter writer = new StreamWriter("Assets/Data/teamNames.txt");
-        for (int i = 0; i < teams.Count; i++)
-        {
-            writer.WriteLine(teamNames[i] + ":" + teamColors[i]);
-            Debug.Log(teamNames[i] + ":" + teamColors[i]);
-        }
-        writer.Close();
+    //    // Writes the selected team names and colors to a file
+    //    StreamWriter writer = new StreamWriter("Assets/Data/teamNames.txt");
+    //    for (int i = 0; i < teams.Count; i++)
+    //    {
+    //        writer.WriteLine(teamNames[i] + ":" + teamColors[i]);
+    //        Debug.Log(teamNames[i] + ":" + teamColors[i]);
+    //    }
+    //    writer.Close();
 
-        //return potentialNames;
-    }
+    //    //return potentialNames;
+    //}
 
-    // Have this return a List<string>
-    public virtual void ReadTeams()
-    {
-        List<string> teamNames = new List<string>();
-        List<string> teamColors = new List<string>();
+    //// Have this return a List<string>
+    //public virtual void ReadTeams()
+    //{
+    //    List<string> teamNames = new List<string>();
+    //    List<string> teamColors = new List<string>();
 
-        // Reads the team names from the file
-        StreamReader reader = new StreamReader("Assets/Data/teamNames.txt");
-        while (reader.Peek() != -1)
-        {
-            string[] line = reader.ReadLine().Split(':');
+    //    // Reads the team names from the file
+    //    StreamReader reader = new StreamReader("Assets/Data/teamNames.txt");
+    //    while (reader.Peek() != -1)
+    //    {
+    //        string[] line = reader.ReadLine().Split(':');
 
-            teamNames.Add(line[0]);
-            Debug.Log(line[0]);
-            teamColors.Add(line[1]);
-            Debug.Log(line[1]);
-        }
-        reader.Close();
+    //        teamNames.Add(line[0]);
+    //        Debug.Log(line[0]);
+    //        teamColors.Add(line[1]);
+    //        Debug.Log(line[1]);
+    //    }
+    //    reader.Close();
 
-        Color readColor;
+    //    Color readColor;
 
-        for(int i = 0; i < teams.Count; i++)
-        {
-            ColorUtility.TryParseHtmlString(teamColors[i], out readColor);
+    //    for(int i = 0; i < teams.Count; i++)
+    //    {
+    //        ColorUtility.TryParseHtmlString(teamColors[i], out readColor);
 
-            teams[i].TeamName = teamNames[i];
-            teams[i].TeamColor = readColor;
-        }
+    //        teams[i].TeamName = teamNames[i];
+    //        teams[i].TeamColor = readColor;
+    //    }
 
-        //return teamNames;
-    }
+    //    //return teamNames;
+    //}
 }
