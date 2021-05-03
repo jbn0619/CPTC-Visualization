e594d4ab2c51da299622c9616df714dceaa71521
Kevin Laporte <kjl8898@rit.edu>
2021-05-02 20:48:54 -0400

    Started work on spoofing data in the Controller Scene

---
+++ CPTC Visualizer Unity Project/CPTC Visualizer/Assets/Scripts/ControllerScene/DataPuller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPuller: MonoBehaviour
+{
+    #region Fields
+    [SerializeField]
+    private FileManager fileManager;
+
+    [SerializeField]
+    private float pullInterval;
+    [SerializeField]
+    private float pullTimer;
+
+
+    #endregion Fields
+    
+    #region Properties
+    
+    #endregion Properties
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
